0a32a7c3c4bf9ee422c19947854626f8d18228ef
Edward K. Ream <edreamleo@gmail.com>
2018-02-18 01:27:21 -0600

    proto-leoserver command now calls g.execute_shell_commands_with_options. Improved docstrings.
    Leo build: 20180218012721

--- leo/core/leoPrototype.py
+++ leo/core/leoPrototype.py
@@ -8,7 +8,7 @@ Commands that run various prototype projects.
 The docstrings for various commands are reference documentation.
 '''
 import leo.core.leoGlobals as g
-import os
+# import os
 #@+others
 #@+node:ekr.20180217083208.1: ** @g.command('leo-el-vue')
 @g.command('leo-el-vue')
@@ -23,15 +23,15 @@ def proto_leo_el_vue(event):
         
     @string leo-el-vue-base = None
     
-        The path to the folder containing the leo-el-vue folder.
+        The path to the **base directory**, the leo-el-vue folder.
         
         Defaults to leo/proto/Vitalije/leo-el-vue
         
     @data leo-el-vue-commands
     
         The body text of this setting contains the commands to be executed,
-        one per line. Preceding any command with '&' causes Leo not to wait
-        for the result.
+        one per line, from the base directory. Preceding any command with
+        '&' causes Leo not to wait for the result.
         
         Defaults to &npm run dev
         
@@ -61,16 +61,35 @@ def proto_leoserver(event):
 
     The code is here:
     https://github.com/leo-editor/leo-editor/files/1705639/leoserver.zip
+    
+    The following settings affect this command, with the defaults as shown:
+        
+    @string leoserver-base = None
+    
+        The path to the **base directory**, the leo-el-vue folder.
+        
+        Defaults to leo/proto/Terry/leoserver
+        
+    @data leo-el-vue-commands
+    
+        The body text of this setting contains the commands to be executed,
+        one per line, in the base directory. Preceding any command with '&'
+        causes Leo not to wait for the result.
+        
+        Defaults to &python leoserver.py
     '''
-    # c = event and event.get('c')
-    base = g.os_path_finalize_join(g.app.loadDir,
+    c = event and event.get('c')
+    base_dir = g.os_path_finalize_join(g.app.loadDir,
         '..', 'proto', 'Terry', 'leoserver')
-    base = base.replace('\\','/')
-    if g.os_path_exists(base):
-        g.es_print('WARNING: killing the server will also kill Leo')
-        os.chdir(base) # Can't do this in the commands list.
-        g.execute_shell_commands(commands=['&python leoserver.py',])
-    else:
-        g.es_print('not found: %r' % base)
+    g.execute_shell_commands_with_options(
+        c = c,
+        base_dir=base_dir,
+        commands = ['&python leoserver.py',],
+        command_setting = 'leoserver-commands',
+            # @data leoserver-commands
+        path_setting= 'leoserver-base',
+            # @string leoserver-base
+    )
+
 #@-others
 #@-leo
