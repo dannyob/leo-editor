d95aea7efce3c22287d1a0623ea34fcc55c1d551
Edward K. Ream <edreamleo@gmail.com>
2018-02-18 01:50:19 -0600

    First draft of the leo-cljs command. Not tested
    Leo build: 20180218015020

--- leo/core/leoPrototype.py
+++ leo/core/leoPrototype.py
@@ -10,14 +10,61 @@ The docstrings for various commands are reference documentation.
 import leo.core.leoGlobals as g
 # import os
 #@+others
+#@+node:ekr.20180218012825.1: ** @g.command('leo-cljs') NEW
+@g.command('leo-cljs')
+@g.command('proto-leo-cljs')
+def proto_leo_cljs(event):
+    '''
+    Vitalije's clojurescript project, using electron and Om/React.
+    
+    First announced here:
+    https://groups.google.com/d/msg/leo-editor/Isd93qGd-RU/SaQoVv7gCQAJ
+    
+    Original sources and documentation at:
+    https://repo.computingart.net/leocljs/tree?ci=tip
+    
+    The following settings affect this command, with the defaults as shown:
+        
+    @string leo-cljs-base = None
+    
+        The path to the **base directory**, the leo-el-vue folder.
+        
+        Defaults to leo/proto/Vitalije/leocljs
+        
+    @data leo-el-vue-commands
+    
+        The body text of this setting contains the commands to be executed,
+        one per line, from the base directory. Preceding any command with
+        '&' causes Leo not to wait for the result.
+        
+        Defaults to yarn dev
+        
+        `npm -g yarn` installs yarn globally.
+    '''
+    c = event and event.get('c')
+    base_dir = g.os_path_finalize_join(g.app.loadDir,
+        '..', 'proto', 'Vitalije', 'leocljs')
+    g.execute_shell_commands_with_options(
+        c = c,
+        base_dir=base_dir,
+        commands = ['&yarn',], # &yarn dev ??
+        command_setting = 'leocljs-commands',
+            # @data leo-el-vue-commands
+        path_setting= 'leocljs-base',
+            # @string leo-el-vue-base
+    )
 #@+node:ekr.20180217083208.1: ** @g.command('leo-el-vue')
 @g.command('leo-el-vue')
 @g.command('proto-leo-el-vue')
 def proto_leo_el_vue(event):
     '''
-    Vitalije's electron/vue project:: Leo in CoffeeScript and Vue.
+    Vitalije's electron/vue project: Leo in CoffeeScript and Vue.
     
-    Original sources and documentation at: https://leoelvue.computingart.net/home
+    First announced here:
+    https://groups.google.com/d/msg/leo-editor/Isd93qGd-RU/SaQoVv7gCQAJ
+    
+    Original sources and documentation here:
+    https://leoelvue.computingart.net/home
     
     The following settings affect this command, with the defaults as shown:
         
